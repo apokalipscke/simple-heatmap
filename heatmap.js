f15ef5711219949c9f228306ff1a8e3a78e5d4bd
marlip <ma...ip@online.de>
2016-08-10 21:42:08 +0200

    changed output to svg insead of div elements

--- heatmap.js
+++ heatmap.js
@@ -1,6 +1,8 @@
 var debug = false;
 
 $(document).ready(function() {
+    $.getScript("svg.js");
+
     $(document).click(function(event) {
         //var firstMargin = parseInt($('body>*:first').css('margin-top'));
         var data = {
@@ -21,16 +23,31 @@ $(document).ready(function() {
     });
 });
 
+function drawOverlay(d) {
+    var draw = SVG('drawing').size(document.body.scrollWidth, document.body.scrollHeight);
+    var gradient = draw.gradient('radial', function(stop) {
+        stop.at(0   , '#04A4EE', 0.05);
+        stop.at(1   , '#04A4EE', 0);
+    });
+    //var circle = draw.circle(100).move(100, 100).fill(gradient);
+    var circles = [];
+    d.forEach(function(element, index) {
+        draw.circle(100).move(element.posx-50, element.posy-50).fill(gradient);
+        //console.log(element.posx)
+    });
+}
+
 function getData() {
     debug&&console.log($.now() + " start fetching data");
     $.post("heatmap.php", 'getData', function(textStatus) {
         var data = JSON.parse(textStatus);
         debug&&console.log($.now() + " received data, start to draw");
         //console.log(textStatus);
-        data.forEach(function(element, index) {
+        //data.forEach(function(element, index) {
             //console.log(element);
-            $('<div class="punkt"></div>').css('left',element.posx+'px').css('top',element.posy+'px').appendTo('body');
-        });
+            //$('<div class="punkt"></div>').css('left',element.posx+'px').css('top',element.posy+'px').appendTo('body');
+        //});
+        drawOverlay(data);
         debug&&console.log($.now() + " finished drawing " + data.length + " elements");
     });
     return 1;
--- readme.md
+++ readme.md
@@ -6,4 +6,4 @@ todo
 1. add screen resolutions
 2. userhash
 3. resolve problem of the first elements top-margin
-4. group elements for faster output
+4. cluster elements for faster and better output
